a845cc1c027a9d5e1bd98a562da6823cdec92e32
maosilu <1540688711@qq.com>
2018-02-24 16:00:44 +0800

    在TP5中配置微信公众账号接入API

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -5,6 +5,30 @@ class Index
 {
     public function index()
     {
-        return '<style type="text/css">*{ padding: 0; margin: 0; } .think_default_text{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p> ThinkPHP V5<br/><span style="font-size:30px">十年磨一剑 - 为API开发设计的高性能框架</span></p><span style="font-size:22px;">[ V5.0 版本由 <a href="http://www.qiniu.com" target="qiniu">七牛云</a> 独家赞助发布 ]</span></div><script type="text/javascript" src="https://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script><script type="text/javascript" src="https://e.topthink.com/Public/static/client.js"></script><think id="ad_bd568ce7058a1091"></think>';
+        echo 'hahaha';
+        die;
+        /*
+         * 接入微信开发API
+         * */
+        //1. 将nonce，timestamp，token按字典顺序排序
+        $nonce = $_GET['nonce'];
+        $timestamp = $_GET['timestamp'];
+        $token = 'weChat';
+        $signature = $_GET['signature'];
+        $arr = array($nonce, $timestamp, $token);
+        sort($arr);
+        //2. 将排序后的三个参数拼接之后用sha1()加密
+        $tempStr = implode('', $arr);
+        $tempStr = sha1($tempStr);
+        //3. 将加密后的字符串与signature进行对比，判断该请求是否来自微信
+        if($tempStr == $signature){
+            echo $_GET['echostr'];
+            exit;
+
+        }
+    }
+
+    public function show(){
+        echo 'Hello world';
     }
 }
