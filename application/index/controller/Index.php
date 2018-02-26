6eb4b2cfbc0d4328aee3a5f68a281be084ffe34e
maosilu <1540688711@qq.com>
2018-02-26 11:06:41 +0800

    获取微信推送消息并回复消息

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -22,10 +22,55 @@ class Index
         if($tempStr == $signature){
             echo $_GET['echostr'];
             exit;
-
         }
     }
 
+    /**
+     * 接收事件订阅与回复响应消息
+     * */
+    public function responseMsg(){
+        //1. 获取到微信推送过来的post数据（xml格式）
+        $postArr = $GLOBALS['HTTP_RAW_POST_DATA'];
+        //2. 处理消息类型，并设置回复类型和内容
+        /*<xml>
+        <ToUserName>< ![CDATA[toUser] ]></ToUserName>
+        <FromUserName>< ![CDATA[FromUser] ]></FromUserName>
+        <CreateTime>123456789</CreateTime>
+        <MsgType>< ![CDATA[event] ]></MsgType>
+        <Event>< ![CDATA[subscribe] ]></Event>
+        </xml>*/
+        $postObj = simplexml_load_string($postArr);
+        //判断该数据包是否是subscribe的事件推送
+        if(strtolower($postObj->MsgType) == 'event'){
+            //如果是subscribe事件
+            if(strtolower($postObj->Event) == 'subscribe'){
+                //回复用户消息（text类型）
+                /*<xml>
+                <ToUserName>< ![CDATA[toUser] ]></ToUserName>
+                <FromUserName>< ![CDATA[fromUser] ]></FromUserName>
+                <CreateTime>12345678</CreateTime>
+                <MsgType>< ![CDATA[text] ]></MsgType>
+                <Content>< ![CDATA[你好] ]></Content>
+                </xml>*/
+                $toUser = $postObj->FromUserName;
+                $fromUser = $postObj->ToUserName;
+                $time = time();
+                $msgType = 'text';
+                $content = '欢迎关注我们的微信公众号。';
+                $template = "<xml>
+                <ToUserName>< ![CDATA[%s] ]></ToUserName>
+                <FromUserName>< ![CDATA[%s] ]></FromUserName>
+                <CreateTime>%d</CreateTime>
+                <MsgType>< ![CDATA[%s] ]></MsgType>
+                <Content>< ![CDATA[%s] ]></Content>
+                </xml>";
+                $info = sprintf($template, $toUser, $fromUser, $time, $msgType, $content);
+                echo $info;
+            }
+        }
+    }
+
+    //test
     public function show(){
         echo 'Hello world';
     }
