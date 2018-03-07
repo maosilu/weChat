3da5d915bd2800bd06f36b5c04c03ecbe42d8434
maosilu <1540688711@qq.com>
2018-03-07 17:31:35 +0800

    添加model层业务处理

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,6 +1,8 @@
 <?php
 namespace app\index\controller;
 
+use app\index\model;
+
 class Index
 {
     public function index()
@@ -113,37 +115,9 @@ class Index
         if($keyword == 'hello'){
             $content = 'hello world';
         }elseif ($keyword == '图文'){
-            $title1 = '我的CSDN';
-            $description1 = '我是美女';
-            $picurl1 = 'http://'.$_SERVER['HTTP_HOST'].'/weChat/public/static/image/big_spring.jpeg';
-            $url1 = 'http://blog.csdn.net/maosilu_ICE';
-            $title2 = '我的开源中国';
-            $description2 = '我是才女';
-            $picurl2 = 'http://'.$_SERVER['HTTP_HOST'].'/weChat/public/static/image/small_spring.jpg';
-            $url2 = 'https://my.oschina.net/maosilu/blog';
-            $template = '<xml>
-        <ToUserName><![CDATA[%s]]></ToUserName>
-        <FromUserName><![CDATA[%s]]></FromUserName>
-        <CreateTime>%s</CreateTime>
-        <MsgType><![CDATA[news]]></MsgType>
-        <ArticleCount>2</ArticleCount>
-        <Articles>
-            <item>
-                <Title><![CDATA[%s]]></Title>
-                <Description><![CDATA[%s]]></Description>
-                <PicUrl><![CDATA[%s]]></PicUrl>
-                <Url><![CDATA[%s]]></Url>
-            </item>
-            <item>
-                <Title><![CDATA[%s]]></Title>
-                <Description><![CDATA[%s]]></Description>
-                <PicUrl><![CDATA[%s]]></PicUrl>
-                <Url><![CDATA[%s]]></Url>
-            </item>
-        </Articles>
-        </xml>';
-            $info = sprintf($template, $toUser, $fromUser, time(), $title1, $description1, $picurl1, $url1, $title2, $description2, $picurl2, $url2);
-            return $info;
+//            (new \app\index\model\Index())->responseGraphic();
+            return (new model\Index)->responseGraphic($postObj);
+            
         }else{
             $content = date('Y-m-d H:i:s', time())."\r\n".'<a href="https://github.com/maosilu/weChat">代码git地址</a>'."\r\n嘣嘣嘣嘣！～";
         }
---
+++ application/index/model/Index.php
@@ -0,0 +1,43 @@
+<?php
+namespace app\index\model;
+
+use think\Model;
+
+class Index extends Model
+{
+    public function responseGraphic($postObj){
+        $toUser = $postObj->FromUserName;
+        $fromUser = $postObj->ToUserName;
+        $title1 = '我的CSDN';
+        $description1 = '我是美女';
+        $picurl1 = 'http://'.$_SERVER['HTTP_HOST'].'/weChat/public/static/image/big_spring.jpeg';
+        $url1 = 'http://blog.csdn.net/maosilu_ICE';
+        $title2 = '我的开源中国';
+        $description2 = '我是才女';
+        $picurl2 = 'http://'.$_SERVER['HTTP_HOST'].'/weChat/public/static/image/small_spring.jpg';
+        $url2 = 'https://my.oschina.net/maosilu/blog';
+        $template = '<xml>
+        <ToUserName><![CDATA[%s]]></ToUserName>
+        <FromUserName><![CDATA[%s]]></FromUserName>
+        <CreateTime>%s</CreateTime>
+        <MsgType><![CDATA[news]]></MsgType>
+        <ArticleCount>2</ArticleCount>
+        <Articles>
+            <item>
+                <Title><![CDATA[%s]]></Title>
+                <Description><![CDATA[%s]]></Description>
+                <PicUrl><![CDATA[%s]]></PicUrl>
+                <Url><![CDATA[%s]]></Url>
+            </item>
+            <item>
+                <Title><![CDATA[%s]]></Title>
+                <Description><![CDATA[%s]]></Description>
+                <PicUrl><![CDATA[%s]]></PicUrl>
+                <Url><![CDATA[%s]]></Url>
+            </item>
+        </Articles>
+        </xml>';
+        $info = sprintf($template, $toUser, $fromUser, time(), $title1, $description1, $picurl1, $url1, $title2, $description2, $picurl2, $url2);
+        return $info;
+    }
+}
